0dfa6c04bfba904422c8286d3b05d542e898834a
MervinLuo <Mervin_Luo@outlook.com>
2025-07-09 10:48:40 +0800

    2025.7.9(2)

--- CodeForces/Codeforces Round 1035 (Div. 2)/D.cpp
+++ CodeForces/Codeforces Round 1035 (Div. 2)/D.cpp
@@ -12,7 +12,7 @@ void solve(){
     
 }
 signed main(){
-    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0); 
     int t;
     cin >> t;
     while(t--){
