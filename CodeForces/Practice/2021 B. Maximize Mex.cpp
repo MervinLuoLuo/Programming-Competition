fd32a16e101eb9fb29dbaf59771ea3db882fcf62
MervinLuo <Mervin_Luo@outlook.com>
2025-07-13 22:42:19 +0800

    2025.7.13

--- CodeForces/Practice/2021 B. Maximize Mex.cpp
+++ CodeForces/Practice/2021 B. Maximize Mex.cpp
@@ -10,7 +10,23 @@ const int INF = 1e18;
 const int maxn = 1e9;
 
 void solve(){
-    
+    int n,x;cin >> n >> x;
+    map<int,int> a;
+    for(int i = 0; i < n;i++){
+        int r;cin >> r;
+        a[r]++;
+    }
+    for(auto it : a){
+        if(it.second > 1){
+            a[it.first + x] += it.second - 1;
+            it.second = 1;
+        }
+    }
+    int i = 0;
+    for(; i < 1e9; i++){
+        if(a[i] == 0) break;
+    }
+    cout << i << endl;
 }
 signed main(){
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
