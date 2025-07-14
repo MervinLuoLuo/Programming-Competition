06b964a8fbd3a3b149cb187b7ac1e7ac3e8deb9f
MervinLuo <Mervin_Luo@outlook.com>
2025-07-14 20:07:34 +0800

    2025.7.14

--- CodeForces/Practice/2019 B. All Pairs Segments.cpp
+++ CodeForces/Practice/2019 B. All Pairs Segments.cpp
@@ -1,4 +1,4 @@
-/*https://codeforces.com/problemset/problem/2020/B*/
+/*https://codeforces.com/problemset/problem/2019/B*/
 #include <bits/stdc++.h>
 #define endl '\n'
 #define yes cout << "Yes" << endl
@@ -10,15 +10,7 @@ const int INF = 1e18;
 const int maxn = 1e9;
 
 void solve(){
-    int k;cin >> k;
-    int l = 1, r = 2e18;
-    while(r - 1 > l){
-        int mid = l + ((r - l) >> 1);
-        int n = mid - signed(sqrtl(mid));
-        if(n >= k) r = mid;
-        else l = mid;
-    }
-    cout << r << endl;
+    
 }
 signed main(){
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
