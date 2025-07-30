bd91e5bfd9266019a66cb6c8f5a194822dd03caa
MervinLuo <Mervin_Luo@outlook.com>
2025-07-30 22:00:27 +0800

    2025.7.30

---
+++ CodeForces/Practice/1987 C. Basil's Garden.cpp
@@ -0,0 +1,29 @@
+/*https://codeforces.com/problemset/problem/1987/C*/
+#include <bits/stdc++.h>
+#define endl '\n'
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define int long long
+#define pii pair<int,int>
+#define vii vector<vector<int>>
+using namespace std;
+const int INF = 1e18;
+const int maxn = 1e9;
+
+void solve(){
+    int n;cin >> n;
+    vector<int> a(n);
+    for(int &x : a) cin >> x;
+    int ans = a[n - 1];
+    for(int i = n - 2; i >= 0; i--) ans = max(ans + 1,a[i]);
+    cout << ans << endl;
+}
+signed main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
