f8aa68db1fa15e6fa78cd4bca76d75eb657d1219
MervinLuo <Mervin_Luo@outlook.com>
2025-08-16 17:57:23 +0800

    2025.8.16.3

---
+++ Practice/test.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define int long long
+#define pii pair<int,int>
+#define vii vector<vector<int>>
+using namespace std;
+const int INF = 1e18;
+const int maxn = 1e9;
+
+void solve(){
+    cout << "a" << endl;
+}
+signed main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    solve();
+    return 0;
+}
