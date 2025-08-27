5b5ca33d51944bf0e5f458b5d042aa547548983d
MervinLuo <Mervin_Luo@outlook.com>
2025-08-27 22:40:48 +0800

    2025.8.27.1

---
+++ CodeForces/Codeforces Round 1045 (Div. 2)/A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define int long long
+#define pii pair<int,int>
+#define vii vector<vector<int>>
+using namespace std;
+const int INF = 1e18;
+const int maxn = 1e9;
+
+void solve(){
+    int n,a,b;cin >> n >> a >> b;
+    if(a <= b){
+        if((n & 1) == (b & 1)) yes;
+        else no;
+    }
+    else {
+        if((n & 1) == (a & 1) && (b & 1) == (n & 1)) yes;
+        else no;
+    }
+}
+signed main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
---
+++ CodeForces/Codeforces Round 1045 (Div. 2)/B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define int long long
+#define pii pair<int,int>
+#define vii vector<vector<int>>
+using namespace std;
+const int INF = 1e18;
+const int maxn = 1e9;
+
+void solve(){
+    int n,k;cin >> n >> k;
+    vector<int> a(n);
+    for(int &x : a) cin >> x;
+
+    for(int i = 0; i < n; i++){
+        a[i] += (a[i] % (k + 1) ) * k;
+    }
+
+    for(auto it : a) cout << it << " ";
+    cout << endl;
+}
+signed main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
