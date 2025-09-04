3ce7e29ecbe45b3e9b606237e337c0e2fc095fd1
MervinLuo <Mervin_Luo@outlook.com>
2025-09-04 23:40:09 +0800

    2025.9.4.4

---
+++ CodeForces/Practice/A. Double Perspective.cpp
@@ -0,0 +1,24 @@
+/*https://codeforces.com/problemset/problem/2129/A*/
+#include <bits/stdc++.h>
+#define endl '\n'
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define int long long
+#define pii pair<int,int>
+#define vii vector<vector<int>>
+using namespace std;
+const int INF = 1e18;
+const int maxn = 1e9;
+
+void solve(){
+    
+}
+signed main(){
+    ios::sync_with_stdio(0);cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
