1948e899c611daeedc8784c46324c49094b8950c
MervinLuo <Mervin_Luo@outlook.com>
2025-09-08 13:03:16 +0800

    2025.9.8.2

--- CodeForces/Codeforces Round 1047 (Div. 3)/C.cpp
+++ CodeForces/Codeforces Round 1047 (Div. 3)/C.cpp
@@ -9,27 +9,13 @@ using namespace std;
 const int INF = 1e18;
 const int maxn = 1e9;
 
-int calc(int x){
-    int res = 0;
-    for(int i = 1; i * i <= x; i++){
-        if(x % i == 0){
-            int f1 =i,f2 = x / i;
-            if(f1 % 2 == 0 && (x / f1) % 2 == 0) res = max(res,f1);
-            if(f2 % 2 == 0 && (x / f2) % 2 == 0) res = max(res,f2);
-        }
-    }
-    return res;
-}
-
 void solve(){
     int a,b;cin >> a >> b;
-    if(b == 1) {cout << -1 << endl;return;}
-    if(!(a & 1) && !(b & 1)) cout << a * b / 2 + 2 << endl;
-    else if((a & 1) && (b & 1)) cout << a * b + 1 << endl;
-    else if((a & 1) && !(b & 1)){
-        int x = calc(b);
-        if(x == 0) cout << -1 << endl;
-        else cout << a * x + b / x << endl;
+    if((a & 1) && (b & 1)) cout << a * b + 1 << endl;
+    else if(!(b & 1)){
+        int ans = a * b / 2 + 2;
+        if(ans & 1) cout << -1 << endl;
+        else cout << ans << endl;
     }
     else if(!(a & 1) && (b & 1)) cout << -1 << endl;
 }
