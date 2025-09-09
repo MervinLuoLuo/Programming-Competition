bc5695a00995c85f0176404ddef765d63abd60b6
MervinLuo <Mervin_Luo@outlook.com>
2025-09-09 19:57:05 +0800

    2025.9.9.2

--- CodeForces/Codeforces Round 1047 (Div. 3)/E.cpp
+++ CodeForces/Codeforces Round 1047 (Div. 3)/E.cpp
@@ -10,7 +10,32 @@ const int INF = 1e18;
 const int maxn = 1e9;
 
 void solve(){
-    
+    int n,k;cin >> n >> k;
+    vector<int> a(n);
+    for(int &x : a) cin >> x;
+    if(k > 1) k = k % 2 + 2;
+
+    for(int i = 0; i< k; i++){
+        vector<int> vis(n + 1,0);
+        for(int i = 0; i < n; i++) vis[a[i]]++;
+
+        // 找MEX
+        int MEX = -1;
+        for(int j = 0; j <= n; j++){
+            if(!vis[j]){
+                MEX = j;
+                break;
+            }
+        }
+
+        for(int j = 0; j < n; j++){
+            if(vis[a[j]] == 1) a[j] = min(a[j],MEX);
+            else a[j] = MEX;
+        }
+    }
+
+    int ans = accumulate(a.begin(),a.end(),0LL);
+    cout << ans << endl;
 }
 signed main(){
     ios::sync_with_stdio(0);cin.tie(0);
