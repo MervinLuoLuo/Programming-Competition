0f04e978b008da351bf961aa1d3c5af9c9b747ee
MervinLuo <124155501+MervinLuoLuo@users.noreply.github.com>
2025-09-11 03:30:43 +0000

    2025.9.11.1

--- CodeForces/Codeforces Round 1049 (Div. 2)/C.cpp
+++ CodeForces/Codeforces Round 1049 (Div. 2)/C.cpp
@@ -10,7 +10,34 @@ const int INF = 1e18;
 const int maxn = 1e9;
 
 void solve(){
-    
+    int n;cin >> n;
+    vector<int> a(n);
+    for(int &x : a) cin >> x;
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        if(i & 1) ans -= a[i];
+        else ans += a[i];
+    }
+
+    int base = ans;
+    for(int i = 0; i < n; i++){
+        ans = max(ans, base + i - (i & 1));
+    }
+
+    int mine = LLONG_MAX >> 2, mino = LLONG_MAX >> 2;
+
+    for(int i = 0;  i < n ; i++){
+        if(i & 1){
+            ans = max(ans, base + i + a[i] + a[i] - mine);
+            mino = min(mino,i - a[i] - a[i]);
+        }
+        else{
+            ans = max(ans,base + i - a[i] - a[i] - mino);
+            mine = min(mine,i + a[i] + a[i]);
+        }
+
+    }
+    cout << ans << endl;
 }
 signed main(){
     ios::sync_with_stdio(0);cin.tie(0);
---
+++ CodeForces/Codeforces Round 1049 (Div. 2)/D.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define int long long
+#define pii pair<int,int>
+#define vii vector<vector<int>>
+using namespace std;
+const int INF = 1e18;
+const int maxn = 1e9;
+
+void solve(){
+    int n;cin >> n;
+    vector<pii> L(n), R(n);
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        int l,r;cin >> l >> r;
+        L[i] = {l,i}; R[i] = {r,i};
+        ans += r - l;
+    }
+
+    sort(L.begin(),L.end(),auto[&](const pii &a,const pii &b){
+
+    });
+}
+signed main(){
+    ios::sync_with_stdio(0);cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
