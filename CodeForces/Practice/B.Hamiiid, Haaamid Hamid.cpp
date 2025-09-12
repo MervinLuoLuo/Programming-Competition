b06b7753b4eb33552e6f7897c45cdb5e7a863b74
MervinLuo <Mervin_Luo@outlook.com>
2025-09-12 20:45:56 +0800

    2025.9.12.2

--- CodeForces/Practice/B.Hamiiid, Haaamid Hamid.cpp
+++ CodeForces/Practice/B.Hamiiid, Haaamid Hamid.cpp
@@ -11,10 +11,25 @@ const int INF = 1e18;
 const int maxn = 1e9;
 
 void solve(){
-    
+    int n,x;cin >> n >> x;
+    string s;cin >> s;
+    if(x == 1 || x == n){cout << 1 << endl;return;}
+    x--;
+    int lf = -maxn,rf = maxn;
+    for(int i = x- 1; i >= 0; i--){
+        if(s[i] == '#') {lf = i;break;}
+    }
+
+    for(int i = x + 1; i < n; i++){
+        if(s[i] == '#') {rf = i;break;}
+    }
+
+    if(lf == -maxn && rf == maxn){cout << 1 << endl;return;}
+
+    cout << max(min(x + 1, n - rf + 1), min(lf + 2, n - x)) << endl;
 }
 signed main(){
-    ios::sync_with_stdio(0);cin.tie(0);
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     int t;
     cin >> t;
     while(t--){
