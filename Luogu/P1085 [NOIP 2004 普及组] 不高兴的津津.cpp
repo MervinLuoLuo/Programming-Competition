6630ca978b11288e2e5cbb169a8ad31f60cf9110
MervinLuo <Mervin_Luo@outlook.com>
2025-09-23 23:12:17 +0800

    2025.9.23.1

--- Luogu/P1085 [NOIP 2004 普及组] 不高兴的津津.cpp
+++ Luogu/P1085 [NOIP 2004 普及组] 不高兴的津津.cpp
@@ -10,7 +10,17 @@ const int INF = 1e18;
 const int maxn = 1e9;
 
 void solve(){
-    cout << 3 % 1 << endl;
+    int nh = 0,maxd = 0;
+    for(int i = 1; i <= 7; i++){
+        int x,y;cin >> x >> y;
+        if(x + y > 8){
+            int res  =x + y - 8;
+            if(res > nh){   
+                nh = res;maxd = i;
+            }
+        }
+    }   
+    cout << maxd << endl;
 }
 signed main(){
     ios::sync_with_stdio(0);cin.tie(0);
